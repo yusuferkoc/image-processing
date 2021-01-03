46800029da29be99ad533cb34848a6afda878cbc
yusuf <yusuferkoc99@gmail.com>
2021-01-03 23:30:24 +0300

    init

---
+++ imaj/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace imaj
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(530, 697);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(582, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(590, 697);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(239, 721);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Orijinal";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(879, 721);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "İşlenmiş";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.ForestGreen;
+            this.button1.Location = new System.Drawing.Point(1244, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Resim Aç";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.ForestGreen;
+            this.button2.Location = new System.Drawing.Point(1244, 506);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 48);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Uygula";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Griye Dönüştür",
+            "Bölge Al",
+            "Histogram Oluştur"});
+            this.comboBox1.Location = new System.Drawing.Point(1218, 237);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(161, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(1240, 358);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 22);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Morfolojik";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Location = new System.Drawing.Point(1240, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 22);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Filtreler";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label5.Location = new System.Drawing.Point(1240, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 22);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ön İşlem";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            " Bulanıklaştırma filtresi",
+            " Keskinleştirme filtresi",
+            "Kenar bulma filtresi"});
+            this.comboBox2.Location = new System.Drawing.Point(1218, 312);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(161, 21);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            " Siyah beyaz resimde genişletme",
+            " Siyah beyaz resimde erozyon"});
+            this.comboBox3.Location = new System.Drawing.Point(1218, 394);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(161, 21);
+            this.comboBox3.TabIndex = 11;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            " Siyah beyaz resimde 4’lü komşuluk ile nesne bulma ve",
+            "gösterme",
+            " Gri seviye resimde istenilen bir yöntemle nesne bulma",
+            "ve gösterme",
+            " Renkli resimde istenilen bir yöntemle nesne bulma ve",
+            "gösterme"});
+            this.comboBox4.Location = new System.Drawing.Point(1218, 471);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(161, 21);
+            this.comboBox4.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Helvetica", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label6.Location = new System.Drawing.Point(1228, 437);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 22);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Segmentasyon";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1406, 752);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ imaj/Form1.cs
@@ -0,0 +1,422 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imaj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofile = new OpenFileDialog();
+            ofile.Filter = "resimler(*.jpg,*.png,*.bmp)|*.jpg;*.png;*.bmp";
+            if (ofile.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+            pictureBox1.ImageLocation = ofile.FileName;
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private Bitmap gray(Bitmap bmp)
+        {
+            for (int i = 0; i < bmp.Height - 1; i++)
+                for (int j = 0; j < bmp.Width - 1; j++)
+                {
+                    int value = (bmp.GetPixel(j, i).R + bmp.GetPixel(j, i).G + bmp.GetPixel(j, i).B) / 3;
+                    Color renk;
+                    renk = Color.FromArgb(value, value, value);
+
+                    bmp.SetPixel(j, i, renk);
+
+                }
+
+            return bmp;
+        }
+        private Bitmap sharp(Bitmap bmp)
+        {
+            int filterWidth = 3;
+            int filterHeight = 3;
+            double[,] filter = new double[filterWidth, filterHeight];
+            filter[0, 0] = filter[0, 1] = filter[0, 2] = filter[1, 0] = filter[1, 2] = filter[2, 0] = filter[2, 1] = filter[2, 2] = -1;
+            filter[1, 1] = 9;
+            double factor = 1.0;
+            double bias = 0.0;
+            Color[,] result = new Color[bmp.Width, bmp.Height];
+            for (int x = 0; x < bmp.Width; ++x)
+            {
+                for (int y = 0; y < bmp.Height; ++y)
+                {
+                    double red = 0.0, green = 0.0, blue = 0.0;
+                    Color imageColor = bmp.GetPixel(x, y);
+
+                    for (int filterX = 0; filterX < filterWidth; filterX++)
+                    {
+                        for (int filterY = 0; filterY < filterHeight; filterY++)
+                        {
+                            int imageX = (x - filterWidth / 2 + filterX + bmp.Width) % bmp.Width;
+                            int imageY = (y - filterHeight / 2 + filterY + bmp.Height) % bmp.Height;
+                            red += imageColor.R * filter[filterX, filterY];
+                            green += imageColor.G * filter[filterX, filterY];
+                            blue += imageColor.B * filter[filterX, filterY];
+                        }
+                        int r = Math.Min(Math.Max((int)(factor * red + bias), 0), 255);
+                        int g = Math.Min(Math.Max((int)(factor * green + bias), 0), 255);
+                        int b = Math.Min(Math.Max((int)(factor * blue + bias), 0), 255);
+
+                        result[x, y] = Color.FromArgb(r, g, b);
+                    }
+                }
+            }
+            Bitmap sharpenImage = new Bitmap(bmp.Width, bmp.Height);
+            for (int i = 0; i < bmp.Width; ++i)
+            {
+                for (int j = 0; j < bmp.Height; ++j)
+                {
+                    sharpenImage.SetPixel(i, j, result[i, j]);
+                }
+            }
+            return sharpenImage;
+        }
+        private Bitmap Dilation(Bitmap SrcImage)
+        {
+            Bitmap tempbmp = new Bitmap(SrcImage.Width, SrcImage.Height);
+            BitmapData SrcData = SrcImage.LockBits(new Rectangle(0, 0,
+            SrcImage.Width, SrcImage.Height), ImageLockMode.ReadOnly,
+            PixelFormat.Format24bppRgb);
+
+            BitmapData DestData = tempbmp.LockBits(new Rectangle(0, 0, tempbmp.Width,
+        tempbmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            byte[,] sElement = new byte[5, 5] {
+        {0,0,1,0,0},
+        {0,1,1,1,0},
+        {1,1,1,1,1},
+        {0,1,1,1,0},
+        {0,0,1,0,0}
+    };
+
+            int size = 5;
+            byte max, clrValue;
+            int radius = size / 2;
+            int ir, jr;
+
+            unsafe
+            {
+
+                // Loop for Columns.
+                for (int colm = radius; colm < DestData.Height - radius; colm++)
+                {
+                    // Initialise pointers to at row start.
+                    byte* ptr = (byte*)SrcData.Scan0 + (colm * SrcData.Stride);
+                    byte* dstPtr = (byte*)DestData.Scan0 + (colm * SrcData.Stride);
+
+                    // Loop for Row item.
+                    for (int row = radius; row < DestData.Width - radius; row++)
+                    {
+                        max = 0;
+                        clrValue = 0;
+
+                        // Loops for element array.
+                        for (int eleColm = 0; eleColm < 5; eleColm++)
+                        {
+                            ir = eleColm - radius;
+                            byte* tempPtr = (byte*)SrcData.Scan0 +
+                                ((colm + ir) * SrcData.Stride);
+
+                            for (int eleRow = 0; eleRow < 5; eleRow++)
+                            {
+                                jr = eleRow - radius;
+
+                                // Get neightbour element color value.
+                                clrValue = (byte)((tempPtr[row * 3 + jr] +
+                                    tempPtr[row * 3 + jr + 1] + tempPtr[row * 3 + jr + 2]) / 3);
+
+                                if (max < clrValue)
+                                {
+                                    if (sElement[eleColm, eleRow] != 0)
+                                        max = clrValue;
+                                }
+                            }
+                        }
+
+                        dstPtr[0] = dstPtr[1] = dstPtr[2] = max;
+
+                        ptr += 3;
+                        dstPtr += 3;
+                    }
+                }
+            }
+
+            SrcImage.UnlockBits(SrcData);
+            tempbmp.UnlockBits(DestData);
+
+            // return dilated bitmap.
+            return tempbmp;
+
+
+        }
+        private Bitmap ero(Bitmap SrcImage)
+        {
+            Bitmap tempbmp = new Bitmap(SrcImage.Width, SrcImage.Height);
+            BitmapData SrcData = SrcImage.LockBits(new Rectangle(0, 0,
+            SrcImage.Width, SrcImage.Height), ImageLockMode.ReadOnly,
+            PixelFormat.Format24bppRgb);
+
+            BitmapData DestData = tempbmp.LockBits(new Rectangle(0, 0, tempbmp.Width,
+        tempbmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            byte[,] sElement = new byte[5, 5] {
+        {0,0,1,0,0},
+        {0,1,1,1,0},
+        {1,1,1,1,1},
+        {0,1,1,1,0},
+        {0,0,1,0,0}
+    };
+
+            int size = 5;
+            byte max, clrValue;
+            int radius = size / 2;
+            int ir, jr;
+
+            unsafe
+            {
+
+                // Loop for Columns.
+                for (int colm = radius; colm < DestData.Height - radius; colm++)
+                {
+                    // Initialise pointers to at row start.
+                    byte* ptr = (byte*)SrcData.Scan0 + (colm * SrcData.Stride);
+                    byte* dstPtr = (byte*)DestData.Scan0 + (colm * SrcData.Stride);
+
+                    // Loop for Row item.
+                    for (int row = radius; row < DestData.Width - radius; row++)
+                    {
+                        max = 0;
+                        clrValue = 0;
+
+                        // Loops for element array.
+                        for (int eleColm = 0; eleColm < 5; eleColm++)
+                        {
+                            ir = eleColm - radius;
+                            byte* tempPtr = (byte*)SrcData.Scan0 +
+                                ((colm + ir) * SrcData.Stride);
+
+                            for (int eleRow = 0; eleRow < 5; eleRow++)
+                            {
+                                jr = eleRow - radius;
+
+                                // Get neightbour element color value.
+                                clrValue = (byte)((tempPtr[row * 3 + jr] +
+                                    tempPtr[row * 3 + jr + 1] + tempPtr[row * 3 + jr + 2]) / 3);
+
+                                if (max < clrValue)
+                                {
+                                    if (sElement[eleColm, eleRow] != 0)
+                                        max = clrValue;
+                                }
+                            }
+                        }
+
+                        dstPtr[0] = dstPtr[1] = dstPtr[2] = max;
+
+                        ptr += 3;
+                        dstPtr += 3;
+                    }
+                }
+            }
+
+            SrcImage.UnlockBits(SrcData);
+            tempbmp.UnlockBits(DestData);
+
+            // return dilated bitmap.
+            return tempbmp;
+
+
+        }
+
+
+        private Bitmap blur(Bitmap img)
+        {
+            Int32 avgR = 0, avgG = 0, avgB = 0;
+            Int32 blurPixelCount = 0;
+
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    Color pixel = img.GetPixel(x, y);
+                    avgR += pixel.R;
+                    avgG += pixel.G;
+                    avgB += pixel.B;
+
+                    blurPixelCount++;
+                }
+            }
+
+            avgR = avgR / blurPixelCount;
+            avgG = avgG / blurPixelCount;
+            avgB = avgB / blurPixelCount;
+
+            for (int y = 0; y < img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    img.SetPixel(x, y, Color.FromArgb(avgR, avgG, avgB));
+                }
+            }
+
+            return img;
+        }
+        private Bitmap sobel(Bitmap image)
+        {
+            Bitmap gri = gray(image);
+            Bitmap buffer = new Bitmap(gri.Width, gri.Height);//görüntünün boyutlarına sahip boş görüntü oluşturuyorsun
+            Color renk;
+            int valx, valy, gradient;
+            int[,] GX = new int[3, 3];
+            int[,] GY = new int[3, 3];
+            //Yatay kenar 
+            GX[0, 0] = -1; GX[0, 1] = 0; GX[0, 2] = 1;
+            GX[1, 0] = -2; GX[1, 1] = 0; GX[1, 2] = 2;
+            GX[2, 0] = -1; GX[2, 1] = 0; GX[2, 2] = 1;
+
+
+            //Dikey kenar
+
+            GY[0, 0] = -1; GY[0, 1] = -2; GY[0, 2] = -1;
+            GY[1, 0] = 0; GY[1, 1] = 0; GY[1, 2] = 0;
+            GY[2, 0] = 1; GY[2, 1] = 2; GY[2, 2] = 1;
+
+
+            for (int i = 0; i < image.Height; i++)
+            {
+                for (int j = 0; j < image.Width; j++)
+                {
+                    if (i == 0 || i == gri.Height - 1 || j == 0 || j == gri.Width - 1)
+                    {
+                        renk = Color.FromArgb(255, 255, 255);
+                        buffer.SetPixel(j, i, renk);
+                        valx = 0;
+                        valy = 0;
+                    }
+                    else
+                    {
+                        valx = gri.GetPixel(j - 1, i - 1).R * GX[0, 0]
+                            + gri.GetPixel(j, i - 1).R * GX[0, 1]
+                            + gri.GetPixel(j + 1, i - 1).R * GX[0, 2]
+                            + gri.GetPixel(j - 1, i).R * GX[1, 0]
+                            + gri.GetPixel(j, i).R * GX[1, 1]
+                            + gri.GetPixel(j + 1, i).R * GX[1, 2]
+                            + gri.GetPixel(j - 1, i + 1).R * GX[2, 0]
+                            + gri.GetPixel(j, i + 1).R * GX[2, 1]
+                            + gri.GetPixel(j + 1, i + 1).R * GX[2, 2];
+
+                        valy = gri.GetPixel(j - 1, i - 1).R * GY[0, 0]
+                             + gri.GetPixel(j, i - 1).R * GY[0, 1]
+                             + gri.GetPixel(j + 1, i - 1).R * GY[0, 2]
+                             + gri.GetPixel(j - 1, i).R * GY[1, 0]
+                             + gri.GetPixel(j, i).R * GY[1, 1]
+                             + gri.GetPixel(j + 1, i).R * GY[1, 2]
+                             + gri.GetPixel(j - 1, i + 1).R * GY[2, 0]
+                             + gri.GetPixel(j, i + 1).R * GY[2, 1]
+                             + gri.GetPixel(j + 1, i + 1).R * GY[2, 2];
+
+                        gradient = (int)(Math.Abs(valx) + Math.Abs(valy));
+
+
+                        if (gradient < 0)
+                            gradient = 0;
+                        if (gradient > 255)
+                            gradient = 255;
+
+                        renk = Color.FromArgb(gradient, gradient, gradient);
+                        buffer.SetPixel(j, i, renk);
+
+
+                    }
+                }
+            }
+            return buffer; ;
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                Bitmap image = new Bitmap(pictureBox1.Image);
+                Bitmap gri = gray(image);
+
+                pictureBox2.Image = gri;
+            }
+            if (comboBox2.SelectedIndex == 0)
+            {
+                Bitmap image = new Bitmap(pictureBox1.Image);
+                Bitmap bluur = blur(image);
+
+                pictureBox2.Image = bluur;
+            }
+            if (comboBox2.SelectedIndex == 1)
+            {
+                Bitmap image = new Bitmap(pictureBox1.Image);
+                Bitmap shr = sharp(image);
+
+                pictureBox2.Image = shr;
+            }
+            if (comboBox2.SelectedIndex == 2)
+            {
+                Bitmap image = new Bitmap(pictureBox1.Image);
+                Bitmap sobell = sobel(image);
+
+                pictureBox2.Image = sobell;
+            }
+            if (comboBox3.SelectedIndex == 0)
+            {
+                Bitmap image = new Bitmap(pictureBox1.Image);
+                Bitmap ed = Dilation(image);
+
+                pictureBox2.Image = ed;
+            }
+            if (comboBox3.SelectedIndex == 1)
+            {
+                Bitmap image = new Bitmap(pictureBox1.Image);
+                Bitmap ed = ero(image);
+
+                pictureBox2.Image = ed;
+            }
+        }
+    }
+}
